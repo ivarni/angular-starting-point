9b570f426e69527824fe9e7be4dd66e6b1503728
Ivar Nilsen <post@ivarnilsen.com>
2013-11-09 15:28:56 +0100

    Don't version dist folder

--- client/dist/templates/app.js
+++
@@ -1,8 +0,0 @@
-angular.module('templates.app', ['main.tpl.html']);
-
-angular.module("main.tpl.html", []).run(["$templateCache", function($templateCache) {
-  $templateCache.put("main.tpl.html",
-    "<div>\n" +
-    "    {{message}}\n" +
-    "</div>");
-}]);
